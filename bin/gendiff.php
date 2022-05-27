7079f9620d778c97d9f4ad7318ae0920578aaa25
vqck <toridnc@gmail.com>
2022-05-28 00:31:20 +0300

    add --format

--- bin/gendiff.php
+++ bin/gendiff.php
@@ -24,13 +24,16 @@ if (file_exists($autoloadPath1)) {
 
 const DOC = <<<DOC
 Generate diff
+
 Usage:
 gendiff (-h|--help)
 gendiff (-v|--version)
+gendiff [--format <fmt>] <firstFile> <secondFile>
 
 Options:
 -h --help                     Show this screen
--v --version                  Show version"
+-v --version                  Show version
+--format <fmt>                Report format [default: stylish]
 
 DOC;
 
